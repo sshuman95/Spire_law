c5af8d837332921692ddc3b5c6856e60e67f8248
unknown <sidney.shuman95@gmail.com>
2020-09-06 10:14:55 -0500

    got loop for first 4 pages working

--- spire_law.py
+++ spire_law.py
@@ -6,34 +6,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 browser = webdriver.Chrome("/Users/Buster/chromedriver")
-urls=[]
-final = []
 browser.get("https://www.floridabar.org/directories/find-mbr/")
 time.sleep(3)
 fname_input = browser.find_element_by_id('sFirstname')
 fname_input.send_keys("*")
 btn_array = browser.find_elements_by_class_name("form-btn")
 submit = None
+
 for btn in btn_array:
     if(btn.get_attribute('value') == "Search"):
         submit = btn
     else:
         print("No")
+
 submit.click()
 WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "output")))
-output = browser.find_elements_by_class_name('profile-compact')
 bar_numbers = []
+attorney_names = []
+for i in range(1,5):
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "output")))
+    output = browser.find_elements_by_class_name('profile-compact')
+    for profile in output:
+        attorney_name = profile.find_elements_by_class_name('profile-name')[0].text
+        attorney_names.append(attorney_name)
+        bar_number = profile.find_elements_by_class_name('profile-bar-number')[0].text
+        bar_numbers.append(bar_number)
+    next_page = browser.find_element_by_xpath('//*[@title="next page"]')
+    next_page.click()
 
-for profile in output:
-   bar_number = profile.find_elements_by_class_name('profile-bar-number')[0].text
-   bar_numbers.append(bar_number)
-
-print(bar_numbers)
 browser.close()
 
+big_data = pd.DataFrame()
+big_data["Attorney Name"] = attorney_names
+big_data["Bar Number"] = bar_numbers
 
-
-#vehicle_overviews = soup.select('.vehicle-overview')
-#soup = BeautifulSoup(browser.page_source,"html.parser")
+big_data.to_csv('spire_law.csv', sep='\t')
